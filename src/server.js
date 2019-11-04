da56a98a12ecf2f33fe8e766a0548f73e1a00efe
RodrigoAnanias03 <r.ananias03@gmail.com>
2019-11-04 07:43:15 -0300

    Initial commit

--- src/server.js
+++ src/server.js
@@ -2,8 +2,10 @@ const express = require('express');
 
 const app = express();
 
+//GET, POST, PUT, DELETE
+
 app.get('/', (req, res) => {
-    return res.json({message: "Hello World"})
+    return res.json({ message: "Hello World" })
 })
 
 app.listen(3333);
